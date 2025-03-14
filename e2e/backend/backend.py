47ad3474de243f4960e832d0eeaddbd2c93ed11e
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-03-14 09:41:45 +0100

    first working docker setup

--- e2e/backend/backend.py
+++ e2e/backend/backend.py
@@ -3,6 +3,7 @@ import os
 
 DEFAULT_MESSAGE = os.getenv('FLASK_MESSAGE', 'Hello, World!')
 DEFAULT_PORT = int(os.getenv('FLASK_PORT', 8080))
+DEFAULT_HOST = os.getenv('FLASK_HOST', 'localhost')
 
 app = Flask(__name__)
 
@@ -11,4 +12,4 @@ def hello_world():
     return DEFAULT_MESSAGE
 
 if __name__ == '__main__':
-    app.run(port=DEFAULT_PORT)
+    app.run(host=DEFAULT_HOST, port=DEFAULT_PORT)
